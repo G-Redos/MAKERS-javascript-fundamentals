f947ff9174ad40a9d01f467ea73e8d87acdf899a
g-redos <g.redos@live.com>
2022-10-11 15:27:32 +0000

    test

---
+++ 1_Phase/6_Loops/test.js
@@ -0,0 +1,17 @@
+for (let i = 0 ; i <= 20 ; i++) {
+    if (i % 2 === 0) {
+      console.log('even')
+    }  else {
+      console.log('odd');
+    }
+  }
+
+  
+  for (let i = 0 ; i <= 20 ; i++) {
+    if (i % 2 === 0) {
+      console.log('even')
+    }  else {
+      console.log('odd');
+    }
+  }
+  // Field notes
